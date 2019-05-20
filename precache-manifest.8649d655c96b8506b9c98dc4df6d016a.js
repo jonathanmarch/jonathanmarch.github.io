9e336ee695acc09481043ba91cab9cf41d1fc36d
jon <jon@DESKTOP-BG6109R>
2019-05-20 21:48:39 +0100

    first commit

---
+++ precache-manifest.8649d655c96b8506b9c98dc4df6d016a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed37d4ac8a362a84394e734004d29754",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3175fc254b6430a2b92",
+    "url": "/static/css/main.b1af2494.chunk.css"
+  },
+  {
+    "revision": "299a24a65ee64bab2856",
+    "url": "/static/js/2.c9d684c6.chunk.js"
+  },
+  {
+    "revision": "c3175fc254b6430a2b92",
+    "url": "/static/js/main.40b79923.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
